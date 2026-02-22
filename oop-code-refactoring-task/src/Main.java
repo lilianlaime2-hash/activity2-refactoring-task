cb444ce705a549bada806420fbb281633425813c
Lilian Laime Lucero <Lilian.Laime0725@jala.university>
2026-02-22 16:13:11 -0400

    refactor names and math calculatios, doubles insead of ints

--- oop-code-refactoring-task/src/Main.java
+++ oop-code-refactoring-task/src/Main.java
@@ -16,47 +16,40 @@ public class Main {
         int option = 0;
         System.out.println("----------Calculate area of shapes-----------");
         while (option != EXIT) {
-            System.out.println("1. Rectangle");
-            System.out.println("2. Circle");
-            System.out.println("3. Triangle");
-            System.out.println("4. Square");
-            System.out.println("5. Sphere");
-            System.out.println("6. Cube");
-            System.out.println("7. Exit");
+            printMenu();
             System.out.print("Choose an option: ");
             option = Integer.parseInt(sc.nextLine());
 
             switch(option){
                 case RECTANGLE: {
-                    int width = 0,height = 0;
 
                     System.out.print("Insert side width: ");
-                    width = Integer.parseInt(sc.nextLine());
+                    int width = Integer.parseInt(sc.nextLine());
                     System.out.print("Insert side height: ");
-                    height = Integer.parseInt(sc.nextLine());
+                    int height = Integer.parseInt(sc.nextLine());
                     int area = width * height;
-                    int perimeter = 2 * width * height;
+                    int perimeter = 2 * (width + height);
                     System.out.println("Area: " + area);
                     System.out.println("Perimeter: " + perimeter);
                     }
                     break;
+                
                 case CIRCLE: {
                     System.out.println("Insert radius:");
                     int radius = 0;
                     radius = Integer.parseInt(sc.nextLine());
-                    int area = (int) (4 * Math.PI * Math.pow(radius, 2));
+                    double area = Math.PI * radius * radius;
                     System.out.println("Area: " + area);
-                    int p = (int) (2 * Math.PI * radius);
-                    System.out.println("Perimeter: " + p);
+                    double perimeter = 2 * Math.PI * radius;
+                    System.out.println("Perimeter: " + perimeter);
                     }
                     break;
                 case TRIANGLE: {
                     System.out.println("Insert height");
-                    int height = 0, base = 0;
-                    height = Integer.parseInt(sc.nextLine());
+                    int height = Integer.parseInt(sc.nextLine());
                     System.out.print("Insert base");
-                    base = Integer.parseInt(sc.nextLine());
-                    int area = (int) (0.5 * (Math.sqrt((height * height) - (base * base) / 4 )* base));
+                    int base = Integer.parseInt(sc.nextLine());
+                    double area = 0.5 * base * height;
                     System.out.println("Area:" + area);
                     int perimeter = 2 * (height + base);
                     System.out.println(MessageFormat.format("Perimeter:{0}", perimeter));
@@ -64,37 +57,45 @@ public class Main {
                     break;
                 case SQUARE:{
                     System.out.println("Insert side");
-                    int s = 0;
-                    s = Integer.parseInt(sc.nextLine());
-                    int area = s * s;
+                    int side = Integer.parseInt(sc.nextLine());
+                    int area = side * side;
                     System.out.println("Area: " + area);
-                    int p = s + s +s + s;
-                    System.out.println("Perimeter: " + p);
+                    int perimeter = 4 * side;
+                    System.out.println("Perimeter: " + perimeter);
                     }
                     break;
                 case SPHERE:{
                     System.out.print("Insert radius: ");
-                    int r = 0;
-                    r = Integer.parseInt(sc.nextLine());
-                    int area = (int)(4 *  Math.PI * r * r);
+                    int radius = Integer.parseInt(sc.nextLine());
+                    double area = (4 *  Math.PI * radius * radius);
                     System.out.println("Area: " + area);
-                    int p = (int)(2 * Math.PI * r);
-                    System.out.println("Perimeter: " + p);
+                    double volume = (4.0 / 3) * Math.PI * Math.pow(radius, 3);
+                    System.out.println("Volume: " + volume);
                     }
                     break;
                 case CUBE:{
                     System.out.println("Insert side ");
-                    int s = 0;
-                    s = Integer.parseInt(sc.nextLine());
-                    int area = s * s + s * s + s * s + s * s + s * s + s * s;
-                    int perimeter = s + s + s + s;
+                    int side = 0;
+                    side = Integer.parseInt(sc.nextLine());
+                    int area = 6 * side * side;
+                    int volume = side * side * side;
                     System.out.println("Area: " + area);
-                    System.out.println("Perimeter: " + perimeter);
+                    System.out.println("volume: " + volume);
                     }
                     break;
                 default:
                     break;
             }
         }
+    }
+
+    private static void printMenu(){
+        System.out.println("1. Rectangle");
+        System.out.println("2. Circle");
+        System.out.println("3. Triangle");
+        System.out.println("4. Square");
+        System.out.println("5. Sphere");
+        System.out.println("6. Cube");
+        System.out.println("7. Exit");
     }
 }
