9f11c1dd48d209c8a160f77432ce809e5e9cd94d
Lilian Laime Lucero <Lilian.Laime0725@jala.university>
2026-02-22 16:25:18 -0400

    refactor extract method

--- oop-code-refactoring-task/src/Main.java
+++ oop-code-refactoring-task/src/Main.java
@@ -21,67 +21,23 @@ public class Main {
             option = Integer.parseInt(sc.nextLine());
 
             switch(option){
-                case RECTANGLE: {
-
-                    System.out.print("Insert side width: ");
-                    int width = Integer.parseInt(sc.nextLine());
-                    System.out.print("Insert side height: ");
-                    int height = Integer.parseInt(sc.nextLine());
-                    int area = width * height;
-                    int perimeter = 2 * (width + height);
-                    System.out.println("Area: " + area);
-                    System.out.println("Perimeter: " + perimeter);
-                    }
+                case RECTANGLE:
+                    handleRectangle(sc);
                     break;
-                
-                case CIRCLE: {
-                    System.out.println("Insert radius:");
-                    int radius = 0;
-                    radius = Integer.parseInt(sc.nextLine());
-                    double area = Math.PI * radius * radius;
-                    System.out.println("Area: " + area);
-                    double perimeter = 2 * Math.PI * radius;
-                    System.out.println("Perimeter: " + perimeter);
-                    }
+                case CIRCLE:
+                    handleCircle(sc);
                     break;
-                case TRIANGLE: {
-                    System.out.println("Insert height");
-                    int height = Integer.parseInt(sc.nextLine());
-                    System.out.print("Insert base");
-                    int base = Integer.parseInt(sc.nextLine());
-                    double area = 0.5 * base * height;
-                    System.out.println("Area:" + area);
-                    int perimeter = 2 * (height + base);
-                    System.out.println(MessageFormat.format("Perimeter:{0}", perimeter));
-                    }
+                case TRIANGLE:
+                    handleTriangle(sc);
                     break;
-                case SQUARE:{
-                    System.out.println("Insert side");
-                    int side = Integer.parseInt(sc.nextLine());
-                    int area = side * side;
-                    System.out.println("Area: " + area);
-                    int perimeter = 4 * side;
-                    System.out.println("Perimeter: " + perimeter);
-                    }
+                case SQUARE:
+                    handleSquare(sc);
                     break;
-                case SPHERE:{
-                    System.out.print("Insert radius: ");
-                    int radius = Integer.parseInt(sc.nextLine());
-                    double area = (4 *  Math.PI * radius * radius);
-                    System.out.println("Area: " + area);
-                    double volume = (4.0 / 3) * Math.PI * Math.pow(radius, 3);
-                    System.out.println("Volume: " + volume);
-                    }
+                case SPHERE:
+                    handleSphere(sc);
                     break;
-                case CUBE:{
-                    System.out.println("Insert side ");
-                    int side = 0;
-                    side = Integer.parseInt(sc.nextLine());
-                    int area = 6 * side * side;
-                    int volume = side * side * side;
-                    System.out.println("Area: " + area);
-                    System.out.println("volume: " + volume);
-                    }
+                case CUBE:
+                    handleCube(sc);
                     break;
                 default:
                     break;
@@ -98,4 +54,65 @@ public class Main {
         System.out.println("6. Cube");
         System.out.println("7. Exit");
     }
+
+    private static void handleRectangle (Scanner sc) {
+            System.out.print("Insert side width: ");
+            int width = Integer.parseInt(sc.nextLine());
+            System.out.print("Insert side height: ");
+            int height = Integer.parseInt(sc.nextLine());
+            int area = width * height;
+            int perimeter = 2 * (width + height);
+            System.out.println("Area: " + area);
+            System.out.println("Perimeter: " + perimeter);
+    }
+
+    private static void handleCircle (Scanner sc) {
+        System.out.println("Insert radius:");
+        int radius = 0;
+        radius = Integer.parseInt(sc.nextLine());
+        double area = Math.PI * radius * radius;
+        System.out.println("Area: " + area);
+        double perimeter = 2 * Math.PI * radius;
+        System.out.println("Perimeter: " + perimeter);
+    }
+
+    private static void handleTriangle(Scanner sc){
+        System.out.println("Insert height");
+        int height = Integer.parseInt(sc.nextLine());
+        System.out.print("Insert base");
+        int base = Integer.parseInt(sc.nextLine());
+        double area = 0.5 * base * height;
+        System.out.println("Area:" + area);
+        int perimeter = 2 * (height + base);
+        System.out.println(MessageFormat.format("Perimeter:{0}", perimeter));
+    }
+    private static void handleSquare(Scanner sc) {
+        System.out.println("Insert side");
+        int side = Integer.parseInt(sc.nextLine());
+        int area = side * side;
+        System.out.println("Area: " + area);
+        int perimeter = 4 * side;
+        System.out.println("Perimeter: " + perimeter);
+    }
+
+    private static void handleSphere(Scanner sc) {
+        System.out.print("Insert radius: ");
+        int radius = Integer.parseInt(sc.nextLine());
+        double area = (4 *  Math.PI * radius * radius);
+        System.out.println("Area: " + area);
+        double volume = (4.0 / 3) * Math.PI * Math.pow(radius, 3);
+        System.out.println("Volume: " + volume);
+    }
+
+    private static void handleCube(Scanner sc){
+        System.out.println("Insert side ");
+        int side = 0;
+        side = Integer.parseInt(sc.nextLine());
+        int area = 6 * side * side;
+        int volume = side * side * side;
+        System.out.println("Area: " + area);
+        System.out.println("volume: " + volume);
+    }
+        
+
 }
